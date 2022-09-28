6346c74306b8023a2371284ad9cdc74ee50d4641
clerdson <clerdson@gmail.com>
2022-09-28 14:54:01 -0300

    testar o coracao da minha entidade

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,31 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:cep/data/dto/models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:cep/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  CepDTO entity = CepDTO(
+      cep: '61645060',
+      bairro: 'caucaia',
+      complemento: 'novo ',
+      ddd: 'CE',
+      ibge: '',
+      gia: 'CE',
+      localidade: 'novo',
+      logradouro: 'novo',
+      siafi: 'CE',
+      uf: 'CE');
+  test('not null', () {
+    expect(entity, isNotNull);
+  });
+  test('cep is =', () {
+    expect(entity.cep, '61645060');
+  });
+  test('uf is =', () {
+    expect(entity.uf, 'CE');
   });
 }
